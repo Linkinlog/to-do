2970bda3436548085c26cfc3d94ecf5dd72e1fdb
Linkinlog <dahlton@linkd.dev>
2021-10-07 06:47:57 -0400

    App: added to home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navb from './components/layout/Navb'
+import Home from './components/pages/Home'
+import './App.css';
 const App = () => {
 	return (
 		<>
-		<Navb />
+			<Navb />
+			<Home />
 		</>
 	)
 }
