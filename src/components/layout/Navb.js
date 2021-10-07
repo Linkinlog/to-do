a3bf879f452e48f20296d6ad9586617f46f328d9
Linkinlog <dahlton@linkd.dev>
2021-10-07 06:47:33 -0400

    Nav: swapped button out

--- src/components/layout/Navb.js
+++ src/components/layout/Navb.js
@@ -3,10 +3,9 @@ import React from 'react';
 const Navb = () => {
 	return (
 		<>
-			<button class='btn btn-primary' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasExample' aria-controls='offcanvasExample'>
-				Button with data-bs-target
+			<button class='btn btn-primary m-2' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasExample' aria-controls='offcanvasExample'>
+			<svg width="24px" height="24px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><title>70 Basic icons by Xicons.co</title><path d="M41,14H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,14Z" fill="#6f7380"/><path d="M41,26H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,26Z" fill="#6f7380"/><path d="M41,38H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,38Z" fill="#6f7380"/></svg>
 			</button>
-
 			<div class='offcanvas offcanvas-start' tabindex='-1' id='offcanvasExample' aria-labelledby='offcanvasExampleLabel'>
 				<div class='offcanvas-header'>
 					<h5 class='offcanvas-title' id='offcanvasExampleLabel'>
