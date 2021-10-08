8aeb4878e99a330e4238a6afed32a39e5e1f4df7
Linkinlog <dahlton@linkd.dev>
2021-10-08 16:41:57 -0400

    todoContext: set up state

---
+++ src/context/todoContext/TodoState.js
@@ -0,0 +1,42 @@
+import React, { useReducer } from 'react';
+import todoReducer from './todoReducer';
+import TodoContext from './todoContext';
+
+const TodoState = ({ children }) => {
+	const initialState = {
+		tasks: [
+			{
+				task: 'Finish todo app',
+				priority: 7,
+				created: 'Fri Oct 08 2021',
+			},
+			{
+				task: 'Finish todo app',
+				priority: 7,
+				created: 'Fri Oct 08 2021',
+			},
+			{
+				task: 'Finish todo app',
+				priority: 7,
+				created: 'Fri Oct 08 2021',
+			},
+			{
+				task: 'Finish todo app',
+				priority: 7,
+				created: 'Fri Oct 08 2021',
+			},
+			{
+				task: 'Finish todo app',
+				priority: 7,
+				created: 'Fri Oct 08 2021',
+			},
+		],
+	};
+	const [state, dispatch] = useReducer(todoReducer, initialState);
+	const value = {
+		tasks: state.tasks,
+	};
+	return <TodoContext.Provider value={value}>{children}</TodoContext.Provider>;
+};
+
+export default TodoState;
