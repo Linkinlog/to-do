9e6a3677c1c9acaf37139cc55409d1523d3d27b7
Linkinlog <dahlton@linkd.dev>
2021-10-08 21:18:54 -0400

    App: added todo context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import Navb from './components/layout/Navb'
-import Home from './components/pages/Home'
+import React from 'react';
+import Navb from './components/layout/Navb';
+import Home from './components/pages/Home';
 import './App.css';
+import TodoState from './context/todoContext/TodoState';
 const App = () => {
 	return (
 		<>
-			<Navb />
-			<Home />
+			<TodoState>
+				<Navb />
+				<Home />
+			</TodoState>
 		</>
-	)
-}
+	);
+};
 
-export default App
+export default App;
