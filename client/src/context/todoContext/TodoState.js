245a81c9bb46a115243d503244f2db79618014a8
Linkinlog <dahlton@linkd.dev>
2021-10-10 19:52:18 -0400

    todoContext: added functions to state

--- client/src/context/todoContext/TodoState.js
+++ client/src/context/todoContext/TodoState.js
@@ -1,40 +1,74 @@
 import React, { useReducer } from 'react';
 import todoReducer from './todoReducer';
 import TodoContext from './todoContext';
-
+import { COMPLETETASK, ADDTASK, CLEARCURRENT } from '../types';
 const TodoState = ({ children }) => {
 	const initialState = {
 		tasks: [
 			{
-				title: 'Finish todo app',
+				id: 1,
+				title: 'Finish sdfsdf todo app',
 				priority: 7,
+				category: 'personal',
 				created: 'Fri Oct 08 2021',
+				isComplete: false,
 			},
 			{
-				title: 'Finish todo app',
+				id: 2,
+				title: 'Finishsdfsdfsf todo app',
 				priority: 7,
+				category: 'personal',
 				created: 'Fri Oct 08 2021',
+				isComplete: false,
 			},
 			{
-				title: 'Finish todo app',
+				id: 3,
+				title: 'Finissdfsdfsh todo app',
 				priority: 7,
+				category: 'personal',
 				created: 'Fri Oct 08 2021',
+				isComplete: false,
 			},
 			{
-				title: 'Finish todo app',
+				id: 4,
+				title: 'Finish sdfsdfsdfstodo app',
 				priority: 7,
+				category: 'personal',
 				created: 'Fri Oct 08 2021',
+				isComplete: false,
 			},
 			{
-				title: 'Finish todo app',
+				id: 5,
+				title: 'Finishsdfsfsf todo app',
 				priority: 7,
+				category: 'professional',
 				created: 'Fri Oct 08 2021',
+				isComplete: false,
 			},
 		],
+		current: null
 	};
 	const [state, dispatch] = useReducer(todoReducer, initialState);
+
+	const completeTask = (id) => {
+		dispatch({ type: COMPLETETASK, payload: id });
+	};
+
+	const addTask = (todo) => {
+		// todo maybe auth or something here
+		dispatch({type: ADDTASK, payload: todo})
+	}
+
+	const clearCurrent = () => {
+		dispatch({type: CLEARCURRENT})
+	}
+
 	const value = {
 		tasks: state.tasks,
+		current: state.current,
+		completeTask,
+		clearCurrent,
+		addTask
 	};
 	return <TodoContext.Provider value={value}>{children}</TodoContext.Provider>;
 };
