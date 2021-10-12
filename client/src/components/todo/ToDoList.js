7514104af247546c3f9b49a23abe29c3ff516907
Linkinlog <dahlton@linkd.dev>
2021-10-11 21:25:39 -0400

    Added transitions to tasks

--- client/src/components/todo/ToDoList.js
+++ client/src/components/todo/ToDoList.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import SingleToDo from './SingleToDo';
 import { useTodos } from '../../context/todoContext/todoContext';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 const ToDoList = () => {
-	const { tasks } = useTodos()
+	const { tasks } = useTodos();
 	return (
-		<div className='row row-cols-1 g-3 text-center p-2 todoRow'>
-			{tasks.map((task) => (
-				<SingleToDo task={task} />
-			))}
+		<div className='row row-cols-1 text-center p-2 todoRow'>
+			<TransitionGroup>
+				{tasks?.map(
+					(task) =>
+						!task.isComplete && (
+							<CSSTransition key={task.id} timeout={750} classNames='item'>
+								<SingleToDo task={task} />
+							</CSSTransition>
+						),
+				)}
+			</TransitionGroup>
 		</div>
 	);
 };
